8885b604283e2e19d8ec371f41194554bb57ad96
meghadandapat <megha13dandapat@gmail.com>
2022-03-13 16:21:33 +0530

    user sign up

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
-const gravtar = require("gravatar");
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
