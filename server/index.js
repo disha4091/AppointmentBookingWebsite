747824c6f6fa327a41532b23af07831b1ab26290
meghadandapat <megha13dandapat@gmail.com>
2022-03-13 16:20:07 +0530

    user sign up

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const url = require('./config');
+const url = require("./config");
 const app = express();
-const doctorRouter = require('./routes/doctors');
+const doctorRouter = require("./routes/doctors");
+const userRouter = require("./routes/users");
 
-app.use(bodyParser.json({limit:"30mb", extended: true})) ;
-app.use(bodyParser.urlencoded({limit:"30mb", extended: true})) ;
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = url.MONGODB ;
+const CONNECTION_URL = url.MONGODB;
 
 app.use(express.json());
-app.use('/api/doctors', doctorRouter);
+app.use("/api/doctors", doctorRouter);
+app.use(userRouter);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{ useNewUrlParser:true, useUnifiedTopology:true}).then(()=>app.listen(PORT,()=>console.log(`Server running at port: ${PORT}`))).catch((err)=>console.log(err));
-
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running at port: ${PORT}`))
+  )
+  .catch((err) => console.log(err));
